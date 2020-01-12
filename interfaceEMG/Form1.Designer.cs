61faefb5ffa6d3d63388e528638117b0bdbfc5b9
Harison Phelype <hary_phely@hotmail.com>
2020-01-11 21:07:55 -0300

    Adicionar arquivos de projeto.

---
+++ interfaceEMG/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace interfaceEMG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.g1 = new ZedGraph.ZedGraphControl();
+            this.g2 = new ZedGraph.ZedGraphControl();
+            this.g3 = new ZedGraph.ZedGraphControl();
+            this.g4 = new ZedGraph.ZedGraphControl();
+            this.g5 = new ZedGraph.ZedGraphControl();
+            this.g6 = new ZedGraph.ZedGraphControl();
+            this.g7 = new ZedGraph.ZedGraphControl();
+            this.g8 = new ZedGraph.ZedGraphControl();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(2, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // g1
+            // 
+            this.g1.AccessibleName = "g1";
+            this.g1.AutoScroll = true;
+            this.g1.AutoScrollMinSize = new System.Drawing.Size(1, 0);
+            this.g1.Location = new System.Drawing.Point(2, 27);
+            this.g1.Margin = new System.Windows.Forms.Padding(1);
+            this.g1.Name = "g1";
+            this.g1.ScrollGrace = 0D;
+            this.g1.ScrollMaxX = 0D;
+            this.g1.ScrollMaxY = 0D;
+            this.g1.ScrollMaxY2 = 0D;
+            this.g1.ScrollMinX = 0D;
+            this.g1.ScrollMinY = 0D;
+            this.g1.ScrollMinY2 = 0D;
+            this.g1.Size = new System.Drawing.Size(1600, 95);
+            this.g1.TabIndex = 5;
+            this.g1.UseExtendedPrintDialog = true;
+            // 
+            // g2
+            // 
+            this.g2.AutoScroll = true;
+            this.g2.AutoScrollMinSize = new System.Drawing.Size(1, 0);
+            this.g2.Location = new System.Drawing.Point(2, 124);
+            this.g2.Margin = new System.Windows.Forms.Padding(1);
+            this.g2.Name = "g2";
+            this.g2.ScrollGrace = 0D;
+            this.g2.ScrollMaxX = 0D;
+            this.g2.ScrollMaxY = 0D;
+            this.g2.ScrollMaxY2 = 0D;
+            this.g2.ScrollMinX = 0D;
+            this.g2.ScrollMinY = 0D;
+            this.g2.ScrollMinY2 = 0D;
+            this.g2.Size = new System.Drawing.Size(1600, 95);
+            this.g2.TabIndex = 6;
+            this.g2.UseExtendedPrintDialog = true;
+            // 
+            // g3
+            // 
+            this.g3.AutoScroll = true;
+            this.g3.AutoScrollMinSize = new System.Drawing.Size(1, 0);
+            this.g3.Location = new System.Drawing.Point(2, 221);
+            this.g3.Margin = new System.Windows.Forms.Padding(1);
+            this.g3.Name = "g3";
+            this.g3.ScrollGrace = 0D;
+            this.g3.ScrollMaxX = 0D;
+            this.g3.ScrollMaxY = 0D;
+            this.g3.ScrollMaxY2 = 0D;
+            this.g3.ScrollMinX = 0D;
+            this.g3.ScrollMinY = 0D;
+            this.g3.ScrollMinY2 = 0D;
+            this.g3.Size = new System.Drawing.Size(1600, 95);
+            this.g3.TabIndex = 7;
+            this.g3.UseExtendedPrintDialog = true;
+            this.g3.Load += new System.EventHandler(this.g3_Load);
+            // 
+            // g4
+            // 
+            this.g4.AutoScroll = true;
+            this.g4.AutoScrollMinSize = new System.Drawing.Size(1, 0);
+            this.g4.Location = new System.Drawing.Point(2, 318);
+            this.g4.Margin = new System.Windows.Forms.Padding(1);
+            this.g4.Name = "g4";
+            this.g4.ScrollGrace = 0D;
+            this.g4.ScrollMaxX = 0D;
+            this.g4.ScrollMaxY = 0D;
+            this.g4.ScrollMaxY2 = 0D;
+            this.g4.ScrollMinX = 0D;
+            this.g4.ScrollMinY = 0D;
+            this.g4.ScrollMinY2 = 0D;
+            this.g4.Size = new System.Drawing.Size(1600, 95);
+            this.g4.TabIndex = 8;
+            this.g4.UseExtendedPrintDialog = true;
+            // 
+            // g5
+            // 
+            this.g5.AutoScroll = true;
+            this.g5.AutoScrollMinSize = new System.Drawing.Size(1, 0);
+            this.g5.Location = new System.Drawing.Point(2, 415);
+            this.g5.Margin = new System.Windows.Forms.Padding(1);
+            this.g5.Name = "g5";
+            this.g5.ScrollGrace = 0D;
+            this.g5.ScrollMaxX = 0D;
+            this.g5.ScrollMaxY = 0D;
+            this.g5.ScrollMaxY2 = 0D;
+            this.g5.ScrollMinX = 0D;
+            this.g5.ScrollMinY = 0D;
+            this.g5.ScrollMinY2 = 0D;
+            this.g5.Size = new System.Drawing.Size(1600, 95);
+            this.g5.TabIndex = 9;
+            this.g5.UseExtendedPrintDialog = true;
+            // 
+            // g6
+            // 
+            this.g6.AutoScroll = true;
+            this.g6.AutoScrollMinSize = new System.Drawing.Size(1, 0);
+            this.g6.Location = new System.Drawing.Point(2, 512);
+            this.g6.Margin = new System.Windows.Forms.Padding(1);
+            this.g6.Name = "g6";
+            this.g6.ScrollGrace = 0D;
+            this.g6.ScrollMaxX = 0D;
+            this.g6.ScrollMaxY = 0D;
+            this.g6.ScrollMaxY2 = 0D;
+            this.g6.ScrollMinX = 0D;
+            this.g6.ScrollMinY = 0D;
+            this.g6.ScrollMinY2 = 0D;
+            this.g6.Size = new System.Drawing.Size(1600, 95);
+            this.g6.TabIndex = 10;
+            this.g6.UseExtendedPrintDialog = true;
+            // 
+            // g7
+            // 
+            this.g7.AutoScroll = true;
+            this.g7.AutoScrollMinSize = new System.Drawing.Size(1, 0);
+            this.g7.Location = new System.Drawing.Point(2, 609);
+            this.g7.Margin = new System.Windows.Forms.Padding(1);
+            this.g7.Name = "g7";
+            this.g7.ScrollGrace = 0D;
+            this.g7.ScrollMaxX = 0D;
+            this.g7.ScrollMaxY = 0D;
+            this.g7.ScrollMaxY2 = 0D;
+            this.g7.ScrollMinX = 0D;
+            this.g7.ScrollMinY = 0D;
+            this.g7.ScrollMinY2 = 0D;
+            this.g7.Size = new System.Drawing.Size(1600, 95);
+            this.g7.TabIndex = 11;
+            this.g7.UseExtendedPrintDialog = true;
+            // 
+            // g8
+            // 
+            this.g8.AutoScroll = true;
+            this.g8.AutoScrollMinSize = new System.Drawing.Size(1, 0);
+            this.g8.Location = new System.Drawing.Point(2, 706);
+            this.g8.Margin = new System.Windows.Forms.Padding(1);
+            this.g8.Name = "g8";
+            this.g8.ScrollGrace = 0D;
+            this.g8.ScrollMaxX = 0D;
+            this.g8.ScrollMaxY = 0D;
+            this.g8.ScrollMaxY2 = 0D;
+            this.g8.ScrollMinX = 0D;
+            this.g8.ScrollMinY = 0D;
+            this.g8.ScrollMinY2 = 0D;
+            this.g8.Size = new System.Drawing.Size(1600, 95);
+            this.g8.TabIndex = 12;
+            this.g8.UseExtendedPrintDialog = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 811);
+            this.Controls.Add(this.g8);
+            this.Controls.Add(this.g7);
+            this.Controls.Add(this.g6);
+            this.Controls.Add(this.g5);
+            this.Controls.Add(this.g4);
+            this.Controls.Add(this.g3);
+            this.Controls.Add(this.g2);
+            this.Controls.Add(this.g1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private ZedGraph.ZedGraphControl g2;
+        private ZedGraph.ZedGraphControl g3;
+        private ZedGraph.ZedGraphControl g4;
+        private ZedGraph.ZedGraphControl g1;
+        private ZedGraph.ZedGraphControl g5;
+        private ZedGraph.ZedGraphControl g6;
+        private ZedGraph.ZedGraphControl g7;
+        private ZedGraph.ZedGraphControl g8;
+    }
+}
+
---
+++ interfaceEMG/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace interfaceEMG
+{
+    public partial class Form1 : Form
+    {
+        static int tamanho = 1500;
+
+        double[] x = new double[tamanho];
+
+        double[] y1 = new double[tamanho];
+        double[] y2 = new double[tamanho];
+        double[] y3 = new double[tamanho];
+        double[] y4 = new double[tamanho];
+        double[] y5 = new double[tamanho];
+        double[] y6 = new double[tamanho];
+        double[] y7 = new double[tamanho];
+        double[] y8 = new double[tamanho];
+
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random numAl = new Random();
+
+            
+
+            for(int i = 0; i < tamanho; i++)
+            {
+                x[i] = i;
+                y1[i] = numAl.Next(1, 101);
+                y2[i] = numAl.Next(1, 101);
+                y3[i] = numAl.Next(1, 101);
+                y4[i] = numAl.Next(1, 101);
+                y5[i] = numAl.Next(1, 101);
+                y6[i] = numAl.Next(1, 101);
+                y7[i] = numAl.Next(1, 101);
+                y8[i] = numAl.Next(1, 101);
+            }
+
+            this.configurarCurvas(g1, 1, y1, x);
+            this.configurarCurvas(g2, 2, y2, x);
+            this.configurarCurvas(g3, 3, y3, x);
+            this.configurarCurvas(g4, 4, y4, x);
+            this.configurarCurvas(g5, 5, y5, x);
+            this.configurarCurvas(g6, 6, y6, x);
+            this.configurarCurvas(g7, 7, y7, x);
+            this.configurarCurvas(g8, 8, y8g, x);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void configurarCurvas(ZedGraph.ZedGraphControl gx, int n, double[] yx, double[] xx)
+        {
+            gx.GraphPane.AddCurve("minha curva", xx, yx, Color.Black, ZedGraph.SymbolType.None);
+            gx.GraphPane.XAxis.IsVisible = false;
+
+            //zedGraphControl1.GraphPane.YAxis.IsVisible = false;
+            gx.GraphPane.Title.IsVisible = false;
+            gx.GraphPane.YAxis.Title.FontSpec.Size = 50;
+            gx.GraphPane.YAxis.Title.Text = "Sinal "+n;
+            gx.GraphPane.AxisChange();
+
+            gx.Refresh();
+        }
+
+        private void g3_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
