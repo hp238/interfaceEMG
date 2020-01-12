6b97443e9d496f8138fb4d9fea12e497e33662d1
Harison Phelype <hary_phely@hotmail.com>
2020-01-12 10:08:03 -0300

    Novas abas adicionadas (falta configurar todas)

--- interfaceEMG/Form1.Designer.cs
+++ interfaceEMG/Form1.Designer.cs
@@ -30,6 +30,15 @@
         {
             this.components = new System.ComponentModel.Container();
             this.canaisTab = new System.Windows.Forms.TabPage();
+            this.cbCanal8 = new System.Windows.Forms.CheckBox();
+            this.cbCanal7 = new System.Windows.Forms.CheckBox();
+            this.cbCanal6 = new System.Windows.Forms.CheckBox();
+            this.cbCanal5 = new System.Windows.Forms.CheckBox();
+            this.cbCanal4 = new System.Windows.Forms.CheckBox();
+            this.cbCanal3 = new System.Windows.Forms.CheckBox();
+            this.cbCanal2 = new System.Windows.Forms.CheckBox();
+            this.cbCanal1 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.g8 = new ZedGraph.ZedGraphControl();
             this.g7 = new ZedGraph.ZedGraphControl();
@@ -39,14 +48,32 @@
             this.g3 = new ZedGraph.ZedGraphControl();
             this.g2 = new ZedGraph.ZedGraphControl();
             this.g1 = new ZedGraph.ZedGraphControl();
-            this.tabControl2 = new System.Windows.Forms.TabControl();
-            this.button2 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.c1 = new System.Windows.Forms.TabPage();
+            this.aba1Graph = new ZedGraph.ZedGraphControl();
+            this.c2 = new System.Windows.Forms.TabPage();
+            this.c3 = new System.Windows.Forms.TabPage();
+            this.c4 = new System.Windows.Forms.TabPage();
+            this.c5 = new System.Windows.Forms.TabPage();
+            this.c6 = new System.Windows.Forms.TabPage();
+            this.c7 = new System.Windows.Forms.TabPage();
+            this.c8 = new System.Windows.Forms.TabPage();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.canaisTab.SuspendLayout();
-            this.tabControl2.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.c1.SuspendLayout();
             this.SuspendLayout();
             // 
             // canaisTab
             // 
+            this.canaisTab.Controls.Add(this.cbCanal8);
+            this.canaisTab.Controls.Add(this.cbCanal7);
+            this.canaisTab.Controls.Add(this.cbCanal6);
+            this.canaisTab.Controls.Add(this.cbCanal5);
+            this.canaisTab.Controls.Add(this.cbCanal4);
+            this.canaisTab.Controls.Add(this.cbCanal3);
+            this.canaisTab.Controls.Add(this.cbCanal2);
+            this.canaisTab.Controls.Add(this.cbCanal1);
             this.canaisTab.Controls.Add(this.button2);
             this.canaisTab.Controls.Add(this.button1);
             this.canaisTab.Controls.Add(this.g8);
@@ -64,6 +91,92 @@
             this.canaisTab.TabIndex = 0;
             this.canaisTab.Text = "8 CANAIS";
             this.canaisTab.UseVisualStyleBackColor = true;
+            // 
+            // cbCanal8
+            // 
+            this.cbCanal8.AutoSize = true;
+            this.cbCanal8.Location = new System.Drawing.Point(3, 774);
+            this.cbCanal8.Name = "cbCanal8";
+            this.cbCanal8.Size = new System.Drawing.Size(15, 14);
+            this.cbCanal8.TabIndex = 15;
+            this.cbCanal8.UseVisualStyleBackColor = true;
+            // 
+            // cbCanal7
+            // 
+            this.cbCanal7.AutoSize = true;
+            this.cbCanal7.Location = new System.Drawing.Point(3, 679);
+            this.cbCanal7.Name = "cbCanal7";
+            this.cbCanal7.Size = new System.Drawing.Size(15, 14);
+            this.cbCanal7.TabIndex = 14;
+            this.cbCanal7.UseVisualStyleBackColor = true;
+            // 
+            // cbCanal6
+            // 
+            this.cbCanal6.AutoSize = true;
+            this.cbCanal6.Location = new System.Drawing.Point(3, 584);
+            this.cbCanal6.Name = "cbCanal6";
+            this.cbCanal6.Size = new System.Drawing.Size(15, 14);
+            this.cbCanal6.TabIndex = 13;
+            this.cbCanal6.UseVisualStyleBackColor = true;
+            // 
+            // cbCanal5
+            // 
+            this.cbCanal5.AutoSize = true;
+            this.cbCanal5.Location = new System.Drawing.Point(3, 489);
+            this.cbCanal5.Name = "cbCanal5";
+            this.cbCanal5.Size = new System.Drawing.Size(15, 14);
+            this.cbCanal5.TabIndex = 12;
+            this.cbCanal5.UseVisualStyleBackColor = true;
+            // 
+            // cbCanal4
+            // 
+            this.cbCanal4.AutoSize = true;
+            this.cbCanal4.Location = new System.Drawing.Point(3, 394);
+            this.cbCanal4.Name = "cbCanal4";
+            this.cbCanal4.Size = new System.Drawing.Size(15, 14);
+            this.cbCanal4.TabIndex = 11;
+            this.cbCanal4.UseVisualStyleBackColor = true;
+            // 
+            // cbCanal3
+            // 
+            this.cbCanal3.AutoSize = true;
+            this.cbCanal3.Location = new System.Drawing.Point(3, 299);
+            this.cbCanal3.Name = "cbCanal3";
+            this.cbCanal3.Size = new System.Drawing.Size(15, 14);
+            this.cbCanal3.TabIndex = 10;
+            this.cbCanal3.UseVisualStyleBackColor = true;
+            // 
+            // cbCanal2
+            // 
+            this.cbCanal2.AutoSize = true;
+            this.cbCanal2.Location = new System.Drawing.Point(3, 204);
+            this.cbCanal2.Name = "cbCanal2";
+            this.cbCanal2.Size = new System.Drawing.Size(15, 14);
+            this.cbCanal2.TabIndex = 9;
+            this.cbCanal2.UseVisualStyleBackColor = true;
+            // 
+            // cbCanal1
+            // 
+            this.cbCanal1.AutoSize = true;
+            this.cbCanal1.Location = new System.Drawing.Point(3, 109);
+            this.cbCanal1.Name = "cbCanal1";
+            this.cbCanal1.Size = new System.Drawing.Size(15, 14);
+            this.cbCanal1.TabIndex = 8;
+            this.cbCanal1.UseVisualStyleBackColor = true;
+            this.cbCanal1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(116, 6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 22);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Limpar Abas";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
@@ -207,48 +320,135 @@
             this.g1.UseExtendedPrintDialog = true;
             this.g1.Load += new System.EventHandler(this.g1_Load_1);
             // 
-            // tabControl2
+            // tabControl1
             // 
-            this.tabControl2.AccessibleName = "";
-            this.tabControl2.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
-            this.tabControl2.Controls.Add(this.canaisTab);
-            this.tabControl2.Location = new System.Drawing.Point(-1, 1);
-            this.tabControl2.Name = "tabControl2";
-            this.tabControl2.SelectedIndex = 0;
-            this.tabControl2.Size = new System.Drawing.Size(1240, 820);
-            this.tabControl2.TabIndex = 0;
+            this.tabControl1.AccessibleName = "";
+            this.tabControl1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.tabControl1.Controls.Add(this.canaisTab);
+            this.tabControl1.Controls.Add(this.c1);
+            this.tabControl1.Controls.Add(this.c2);
+            this.tabControl1.Controls.Add(this.c3);
+            this.tabControl1.Controls.Add(this.c4);
+            this.tabControl1.Controls.Add(this.c5);
+            this.tabControl1.Controls.Add(this.c6);
+            this.tabControl1.Controls.Add(this.c7);
+            this.tabControl1.Controls.Add(this.c8);
+            this.tabControl1.Location = new System.Drawing.Point(-1, 1);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1240, 820);
+            this.tabControl1.TabIndex = 0;
             // 
-            // button2
+            // c1
             // 
-            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Location = new System.Drawing.Point(116, 6);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(107, 22);
-            this.button2.TabIndex = 7;
-            this.button2.Text = "Limpar Abas";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.c1.Controls.Add(this.aba1Graph);
+            this.c1.Location = new System.Drawing.Point(4, 22);
+            this.c1.Name = "c1";
+            this.c1.Size = new System.Drawing.Size(1232, 794);
+            this.c1.TabIndex = 1;
+            this.c1.Text = "Canal 1";
+            this.c1.UseVisualStyleBackColor = true;
+            // 
+            // aba1Graph
+            // 
+            this.aba1Graph.Location = new System.Drawing.Point(15, 4);
+            this.aba1Graph.Name = "aba1Graph";
+            this.aba1Graph.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.aba1Graph.ScrollGrace = 0D;
+            this.aba1Graph.ScrollMaxX = 0D;
+            this.aba1Graph.ScrollMaxY = 0D;
+            this.aba1Graph.ScrollMaxY2 = 0D;
+            this.aba1Graph.ScrollMinX = 0D;
+            this.aba1Graph.ScrollMinY = 0D;
+            this.aba1Graph.ScrollMinY2 = 0D;
+            this.aba1Graph.Size = new System.Drawing.Size(1234, 794);
+            this.aba1Graph.TabIndex = 0;
+            this.aba1Graph.UseExtendedPrintDialog = true;
+            // 
+            // c2
+            // 
+            this.c2.Location = new System.Drawing.Point(4, 22);
+            this.c2.Name = "c2";
+            this.c2.Size = new System.Drawing.Size(1232, 794);
+            this.c2.TabIndex = 2;
+            this.c2.Text = "Canal 2";
+            this.c2.UseVisualStyleBackColor = true;
+            // 
+            // c3
+            // 
+            this.c3.Location = new System.Drawing.Point(4, 22);
+            this.c3.Name = "c3";
+            this.c3.Size = new System.Drawing.Size(1232, 794);
+            this.c3.TabIndex = 3;
+            this.c3.Text = "Canal 3";
+            this.c3.UseVisualStyleBackColor = true;
+            // 
+            // c4
+            // 
+            this.c4.Location = new System.Drawing.Point(4, 22);
+            this.c4.Name = "c4";
+            this.c4.Size = new System.Drawing.Size(1232, 794);
+            this.c4.TabIndex = 4;
+            this.c4.Text = "Canal 4";
+            this.c4.UseVisualStyleBackColor = true;
+            // 
+            // c5
+            // 
+            this.c5.Location = new System.Drawing.Point(4, 22);
+            this.c5.Name = "c5";
+            this.c5.Size = new System.Drawing.Size(1232, 794);
+            this.c5.TabIndex = 5;
+            this.c5.Text = "Canal 5";
+            this.c5.UseVisualStyleBackColor = true;
+            // 
+            // c6
+            // 
+            this.c6.Location = new System.Drawing.Point(4, 22);
+            this.c6.Name = "c6";
+            this.c6.Size = new System.Drawing.Size(1232, 794);
+            this.c6.TabIndex = 6;
+            this.c6.Text = "Canal 6";
+            this.c6.UseVisualStyleBackColor = true;
+            // 
+            // c7
+            // 
+            this.c7.Location = new System.Drawing.Point(4, 22);
+            this.c7.Name = "c7";
+            this.c7.Size = new System.Drawing.Size(1232, 794);
+            this.c7.TabIndex = 7;
+            this.c7.Text = "Canal 7";
+            this.c7.UseVisualStyleBackColor = true;
+            // 
+            // c8
+            // 
+            this.c8.Location = new System.Drawing.Point(4, 22);
+            this.c8.Name = "c8";
+            this.c8.Size = new System.Drawing.Size(1232, 794);
+            this.c8.TabIndex = 8;
+            this.c8.Text = "Canal 8";
+            this.c8.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1264, 811);
-            this.Controls.Add(this.tabControl2);
+            this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.canaisTab.ResumeLayout(false);
-            this.tabControl2.ResumeLayout(false);
+            this.canaisTab.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.c1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TabPage canaisTab;
         private ZedGraph.ZedGraphControl g8;
         private ZedGraph.ZedGraphControl g7;
         private ZedGraph.ZedGraphControl g6;
@@ -257,9 +457,28 @@
         private ZedGraph.ZedGraphControl g3;
         private ZedGraph.ZedGraphControl g2;
         private ZedGraph.ZedGraphControl g1;
-        private System.Windows.Forms.TabControl tabControl2;
+        private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox cbCanal2;
+        private System.Windows.Forms.CheckBox cbCanal1;
+        private System.Windows.Forms.CheckBox cbCanal8;
+        private System.Windows.Forms.CheckBox cbCanal7;
+        private System.Windows.Forms.CheckBox cbCanal6;
+        private System.Windows.Forms.CheckBox cbCanal5;
+        private System.Windows.Forms.CheckBox cbCanal4;
+        private System.Windows.Forms.CheckBox cbCanal3;
+        private System.Windows.Forms.TabPage c1;
+        private System.Windows.Forms.TabPage c2;
+        private ZedGraph.ZedGraphControl aba1Graph;
+        private System.Windows.Forms.TabPage c3;
+        private System.Windows.Forms.TabPage c4;
+        private System.Windows.Forms.TabPage c5;
+        private System.Windows.Forms.TabPage c6;
+        private System.Windows.Forms.TabPage c7;
+        private System.Windows.Forms.TabPage c8;
+        private System.Windows.Forms.TabPage canaisTab;
     }
 }
 
--- interfaceEMG/Form1.cs
+++ interfaceEMG/Form1.cs
@@ -13,7 +13,7 @@ namespace interfaceEMG
     public partial class Form1 : Form
     {
         
-        static int tamanho = 1500;
+        static int tamanho = 2000;
         //eixo x
         double[] x = new double[tamanho];
         //eixo y dos 8 canais
@@ -28,6 +28,8 @@ namespace interfaceEMG
         //TESTE
         bool[] b = new bool[8];
 
+        private readonly Timer timer = new Timer();
+
         //private TabControl tabControl1;
 
         public Form1()
@@ -35,6 +37,7 @@ namespace interfaceEMG
             InitializeComponent();
             //código para sortear os valores de Y dos 8 canais
             Random numAl = new Random();
+            
 
             for (int i = 0; i < tamanho; i++)
             {
@@ -50,48 +53,104 @@ namespace interfaceEMG
             }
 
             //plot cada canal
-            this.configurarCurvas(g1, 1, y1, x);
-            this.configurarCurvas(g2, 2, y2, x);
-            this.configurarCurvas(g3, 3, y3, x);
-            this.configurarCurvas(g4, 4, y4, x);
-            this.configurarCurvas(g5, 5, y5, x);
-            this.configurarCurvas(g6, 6, y6, x);
-            this.configurarCurvas(g7, 7, y7, x);
-            this.configurarCurvas(g8, 8, y8, x);
+            this.configurarCurvas(g1, 1, y1, x, false);
+            this.configurarCurvas(g2, 2, y2, x, false);
+            this.configurarCurvas(g3, 3, y3, x, false);
+            this.configurarCurvas(g4, 4, y4, x, false);
+            this.configurarCurvas(g5, 5, y5, x, false);
+            this.configurarCurvas(g6, 6, y6, x, false);
+            this.configurarCurvas(g7, 7, y7, x, false);
+            this.configurarCurvas(g8, 8, y8, x, false);
+
+            tabControl1.TabPages.Remove(c1);
+            tabControl1.TabPages.Remove(c2);
+            tabControl1.TabPages.Remove(c3);
+            tabControl1.TabPages.Remove(c4);
+            tabControl1.TabPages.Remove(c5);
+            tabControl1.TabPages.Remove(c6);
+            tabControl1.TabPages.Remove(c7);
+            tabControl1.TabPages.Remove(c8);
+
+
         }
 
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
 
-        }
 
-        private void configurarCurvas(ZedGraph.ZedGraphControl gx, int n, double[] yx, double[] xx)
-        {
-            gx.GraphPane.AddCurve("minha curva", xx, yx, Color.Black, ZedGraph.SymbolType.None);
-            gx.GraphPane.XAxis.IsVisible = false;
-            gx.GraphPane.Title.IsVisible = false;
-            gx.GraphPane.YAxis.Title.FontSpec.Size = 50;
-            gx.GraphPane.YAxis.Title.Text = "Sinal "+n;
-            gx.GraphPane.AxisChange();
-            gx.GraphPane.Legend.IsVisible = false;
 
-            gx.Refresh();
-        }
-
-       
 
 
         //botão para adicionar novas abas
         //EM TESTE
         private void button1_Click_1(object sender, EventArgs e)
         {
-            box1 tb = new box1();
-            tb.Show();
-
+            //variável para avaliar se nenhum box foi marcado
+            Boolean aux = false;
+            //verificando cada checkbox
+            if (cbCanal1.Checked)
+            {
+                novaAba(y1,c1);
+                aux = true;
+            }
+            if (cbCanal2.Checked)
+            {
+                novaAba(y2,c2);
+                aux = true;
+            }
+            if (cbCanal3.Checked)
+            {
+                novaAba(y3,c3);
+                aux = true;
+            }
+            if (cbCanal4.Checked)
+            {
+                novaAba(y4,c4);
+                aux = true;
+            }
+            if (cbCanal5.Checked)
+            {
+                novaAba(y5,c5);
+                aux = true;
+            }
+            if (cbCanal6.Checked)
+            {
+                novaAba(y6, c6);
+                aux = true;
+            }
+            if (cbCanal7.Checked)
+            {
+                novaAba(y7, c7);
+                aux = true;
+            }
+            if (cbCanal8.Checked)
+            {
+                novaAba(y8, c8);
+                aux = true;
+            }
+            if (aux == false)
+            {
+                MessageBox.Show("Nenhum checkbox foi marcado.");
+            }
+            /* objForm?.Close();
+             objForm = new box1
+             {
+                 TopLevel = false,
+                 FormBorderStyle = FormBorderStyle.None,
+                 Dock = DockStyle.Fill
+             };
+             */
+            //Form1.CheckForIllegalCrossThreadCalls();
+            //box1 tb = new box1();
+            //tb.Show();
+            //System.Threading.Tasks.Task.Delay(5000);
+            //timer.Stop();
+            //System.Threading.Thread.Sleep(1000);
+            
+            
             //if()
-            /*
+            
             //b = tb.canaisSelecionados();
+            /*
             b[0] = tb.g1;
             
             if (b[0] == true)
@@ -109,14 +168,63 @@ namespace interfaceEMG
         //EM TESTE
         private void button2_Click(object sender, EventArgs e)
         {
-            if (b[0] == true)
+            tabControl1.TabPages.Clear();
+            tabControl1.TabPages.Add(canaisTab);
+        }
+
+
+
+        private void configurarCurvas(ZedGraph.ZedGraphControl gx, int n, double[] yx, double[] xx, Boolean b)
+        {
+
+            gx.GraphPane.AddCurve("minha curva", xx, yx, Color.Black, ZedGraph.SymbolType.None);
+            
+            if(b == false)
             {
-                MessageBox.Show("TRUE");
+                gx.GraphPane.XAxis.IsVisible = false;
+                gx.GraphPane.Title.IsVisible = false;
+                gx.GraphPane.YAxis.Title.FontSpec.Size = 50;
+                gx.GraphPane.YAxis.Title.Text = "Sinal " + n;
+                gx.GraphPane.Legend.IsVisible = false;
             }
             else
             {
-                MessageBox.Show("FALSE");
+                gx.GraphPane.YAxis.Title.Text = "Sinal " + n;
             }
+            
+            gx.GraphPane.AxisChange();
+            
+
+            gx.Refresh();
+        }
+
+        //nova aba
+        private void novaAba(double[] yx, TabPage c)
+        {
+            String test = c.Name.Substring(1);
+            int n = Int16.Parse(test);
+            //string test2 = test.Substring(1);
+            //MessageBox.Show(test2);
+
+            tabControl1.TabPages.Add(c);
+            configurarCurvas(aba1Graph, n, yx, x, true);
+
+            /*
+            aba1Graph.GraphPane.AddCurve(yx);
+            aba1Graph.Refresh();
+            */
+            //String[] nome = new string[1];
+            
+            /*
+            String nome = gx.Name;
+            TabPage nTab = new TabPage();
+            nTab.Text = nome;
+            tabControl1.TabPages.Add(nTab);
+            tabControl1.SelectTab(nTab);
+
+            gx.Refresh();
+            */
+            //MessageBox.Show(nome);
         }
 
         //TESTES
@@ -125,8 +233,8 @@ namespace interfaceEMG
         {
             TabPage canal1Tab = new TabPage();
             canal1Tab.Text = "Nova Guia";
-            tabControl2.TabPages.Add(canal1Tab);
-            tabControl2.SelectTab(canal1Tab);
+            tabControl1.TabPages.Add(canal1Tab);
+            tabControl1.SelectTab(canal1Tab);
 
             ZedGraph.ZedGraphControl gn = new ZedGraph.ZedGraphControl();
             gn.Refresh();
@@ -137,10 +245,18 @@ namespace interfaceEMG
         private void g1_Load(object sender, EventArgs e)
         {
             MessageBox.Show("This is definitely called");
-            box1 g1Jan = new box1();
-            g1Jan.Show();
+            //box1 g1Jan = new box1();
+            //g1Jan.Show();
         }
 
+        private void timer1_Tick(object sender, EventArgs e)
+        {
 
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
