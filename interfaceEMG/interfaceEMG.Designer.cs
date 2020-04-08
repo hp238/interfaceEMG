b1045b8bd890e45b36d0eb851a51ec6b186f8fae
Matheus Vinícius <mvtna@cin.ufpe.br>
2020-04-08 11:33:16 -0300

    Testando relação de gráficos entre as abas

--- interfaceEMG/interfaceEMG.Designer.cs
+++ interfaceEMG/interfaceEMG.Designer.cs
@@ -45,13 +45,13 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.graphCanais = new ZedGraph.ZedGraphControl();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.graphFFT = new ZedGraph.ZedGraphControl();
+            this.graphBars = new ZedGraph.ZedGraphControl();
             this.textBox9 = new System.Windows.Forms.TextBox();
             this.textBox10 = new System.Windows.Forms.TextBox();
             this.timer2 = new System.Windows.Forms.Timer(this.components);
             this.btmTeste = new System.Windows.Forms.Button();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
-            this.zedGraphControl2 = new ZedGraph.ZedGraphControl();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
@@ -60,9 +60,10 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(915, 10);
+            this.comboBox1.Location = new System.Drawing.Point(1220, 12);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(82, 21);
+            this.comboBox1.Size = new System.Drawing.Size(108, 24);
             this.comboBox1.TabIndex = 1;
             // 
             // btmConectar
@@ -71,9 +72,10 @@
             this.btmConectar.FlatAppearance.BorderColor = System.Drawing.Color.Red;
             this.btmConectar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Yellow;
             this.btmConectar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btmConectar.Location = new System.Drawing.Point(1017, 8);
+            this.btmConectar.Location = new System.Drawing.Point(1356, 10);
+            this.btmConectar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btmConectar.Name = "btmConectar";
-            this.btmConectar.Size = new System.Drawing.Size(75, 23);
+            this.btmConectar.Size = new System.Drawing.Size(100, 28);
             this.btmConectar.TabIndex = 2;
             this.btmConectar.Text = "Conectar";
             this.btmConectar.UseVisualStyleBackColor = false;
@@ -84,10 +86,11 @@
             this.tabControl1.Appearance = System.Windows.Forms.TabAppearance.Buttons;
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(12, 39);
+            this.tabControl1.Location = new System.Drawing.Point(16, 48);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1240, 619);
+            this.tabControl1.Size = new System.Drawing.Size(1653, 762);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
@@ -104,10 +107,11 @@
             this.tabPage1.Controls.Add(this.textBox1);
             this.tabPage1.Controls.Add(this.graphCanais);
             this.tabPage1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Location = new System.Drawing.Point(4, 28);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(1232, 590);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage1.Size = new System.Drawing.Size(1645, 730);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Sinais";
             // 
@@ -115,9 +119,10 @@
             // 
             this.textBox8.BackColor = System.Drawing.Color.White;
             this.textBox8.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox8.Location = new System.Drawing.Point(6, 266);
+            this.textBox8.Location = new System.Drawing.Point(8, 327);
+            this.textBox8.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(39, 13);
+            this.textBox8.Size = new System.Drawing.Size(52, 15);
             this.textBox8.TabIndex = 8;
             this.textBox8.Text = "Canal 4";
             // 
@@ -125,9 +130,10 @@
             // 
             this.textBox7.BackColor = System.Drawing.Color.White;
             this.textBox7.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox7.Location = new System.Drawing.Point(6, 556);
+            this.textBox7.Location = new System.Drawing.Point(8, 684);
+            this.textBox7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(39, 13);
+            this.textBox7.Size = new System.Drawing.Size(52, 15);
             this.textBox7.TabIndex = 7;
             this.textBox7.Text = "Canal 8";
             // 
@@ -135,9 +141,10 @@
             // 
             this.textBox6.BackColor = System.Drawing.Color.White;
             this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox6.Location = new System.Drawing.Point(6, 486);
+            this.textBox6.Location = new System.Drawing.Point(8, 598);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(39, 13);
+            this.textBox6.Size = new System.Drawing.Size(52, 15);
             this.textBox6.TabIndex = 6;
             this.textBox6.Text = "Canal 7";
             // 
@@ -145,9 +152,10 @@
             // 
             this.textBox5.BackColor = System.Drawing.Color.White;
             this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox5.Location = new System.Drawing.Point(6, 413);
+            this.textBox5.Location = new System.Drawing.Point(8, 508);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(39, 13);
+            this.textBox5.Size = new System.Drawing.Size(52, 15);
             this.textBox5.TabIndex = 5;
             this.textBox5.Text = "Canal 6";
             // 
@@ -155,9 +163,10 @@
             // 
             this.textBox4.BackColor = System.Drawing.Color.White;
             this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox4.Location = new System.Drawing.Point(6, 337);
+            this.textBox4.Location = new System.Drawing.Point(8, 415);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(39, 13);
+            this.textBox4.Size = new System.Drawing.Size(52, 15);
             this.textBox4.TabIndex = 4;
             this.textBox4.Text = "Canal 5";
             // 
@@ -165,9 +174,10 @@
             // 
             this.textBox3.BackColor = System.Drawing.Color.White;
             this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox3.Location = new System.Drawing.Point(6, 189);
+            this.textBox3.Location = new System.Drawing.Point(8, 233);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(39, 13);
+            this.textBox3.Size = new System.Drawing.Size(52, 15);
             this.textBox3.TabIndex = 3;
             this.textBox3.Text = "Canal 3";
             // 
@@ -175,9 +185,10 @@
             // 
             this.textBox2.BackColor = System.Drawing.Color.White;
             this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox2.Location = new System.Drawing.Point(6, 116);
+            this.textBox2.Location = new System.Drawing.Point(8, 143);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(39, 13);
+            this.textBox2.Size = new System.Drawing.Size(52, 15);
             this.textBox2.TabIndex = 2;
             this.textBox2.Text = "Canal 2";
             // 
@@ -185,16 +196,18 @@
             // 
             this.textBox1.BackColor = System.Drawing.Color.White;
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox1.Location = new System.Drawing.Point(6, 44);
+            this.textBox1.Location = new System.Drawing.Point(8, 54);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(39, 13);
+            this.textBox1.Size = new System.Drawing.Size(52, 15);
             this.textBox1.TabIndex = 1;
             this.textBox1.Text = "Canal 1";
             // 
             // graphCanais
             // 
             this.graphCanais.BackColor = System.Drawing.Color.RosyBrown;
-            this.graphCanais.Location = new System.Drawing.Point(49, 10);
+            this.graphCanais.Location = new System.Drawing.Point(65, 12);
+            this.graphCanais.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.graphCanais.Name = "graphCanais";
             this.graphCanais.ScrollGrace = 0D;
             this.graphCanais.ScrollMaxX = 0D;
@@ -203,7 +216,7 @@
             this.graphCanais.ScrollMinX = 0D;
             this.graphCanais.ScrollMinY = 0D;
             this.graphCanais.ScrollMinY2 = 0D;
-            this.graphCanais.Size = new System.Drawing.Size(1179, 592);
+            this.graphCanais.Size = new System.Drawing.Size(1572, 729);
             this.graphCanais.TabIndex = 0;
             this.graphCanais.UseExtendedPrintDialog = true;
             // 
@@ -211,25 +224,61 @@
             // 
             this.tabPage2.BackColor = System.Drawing.Color.White;
             this.tabPage2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.tabPage2.Controls.Add(this.zedGraphControl2);
-            this.tabPage2.Controls.Add(this.zedGraphControl1);
+            this.tabPage2.Controls.Add(this.graphFFT);
+            this.tabPage2.Controls.Add(this.graphBars);
             this.tabPage2.Controls.Add(this.textBox9);
             this.tabPage2.Controls.Add(this.textBox10);
             this.tabPage2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Location = new System.Drawing.Point(4, 28);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1232, 590);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage2.Size = new System.Drawing.Size(1645, 730);
             this.tabPage2.TabIndex = 0;
             this.tabPage2.Text = "Biofeedback";
+            // 
+            // graphFFT
+            // 
+            this.graphFFT.BackColor = System.Drawing.Color.RosyBrown;
+            this.graphFFT.Location = new System.Drawing.Point(52, 369);
+            this.graphFFT.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.graphFFT.Name = "graphFFT";
+            this.graphFFT.ScrollGrace = 0D;
+            this.graphFFT.ScrollMaxX = 0D;
+            this.graphFFT.ScrollMaxY = 0D;
+            this.graphFFT.ScrollMaxY2 = 0D;
+            this.graphFFT.ScrollMinX = 0D;
+            this.graphFFT.ScrollMinY = 0D;
+            this.graphFFT.ScrollMinY2 = 0D;
+            this.graphFFT.Size = new System.Drawing.Size(1553, 356);
+            this.graphFFT.TabIndex = 12;
+            this.graphFFT.UseExtendedPrintDialog = true;
+            // 
+            // graphBars
+            // 
+            this.graphBars.BackColor = System.Drawing.Color.RosyBrown;
+            this.graphBars.Location = new System.Drawing.Point(53, 4);
+            this.graphBars.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.graphBars.Name = "graphBars";
+            this.graphBars.ScrollGrace = 0D;
+            this.graphBars.ScrollMaxX = 0D;
+            this.graphBars.ScrollMaxY = 0D;
+            this.graphBars.ScrollMaxY2 = 0D;
+            this.graphBars.ScrollMinX = 0D;
+            this.graphBars.ScrollMinY = 0D;
+            this.graphBars.ScrollMinY2 = 0D;
+            this.graphBars.Size = new System.Drawing.Size(1553, 358);
+            this.graphBars.TabIndex = 11;
+            this.graphBars.UseExtendedPrintDialog = true;
             // 
             // textBox9
             // 
             this.textBox9.BackColor = System.Drawing.Color.White;
             this.textBox9.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox9.Location = new System.Drawing.Point(6, 413);
+            this.textBox9.Location = new System.Drawing.Point(8, 508);
+            this.textBox9.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox9.Name = "textBox9";
-            this.textBox9.Size = new System.Drawing.Size(39, 13);
+            this.textBox9.Size = new System.Drawing.Size(52, 15);
             this.textBox9.TabIndex = 9;
             this.textBox9.Text = "FFT";
             // 
@@ -237,9 +286,10 @@
             // 
             this.textBox10.BackColor = System.Drawing.Color.White;
             this.textBox10.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox10.Location = new System.Drawing.Point(6, 189);
+            this.textBox10.Location = new System.Drawing.Point(8, 233);
+            this.textBox10.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox10.Name = "textBox10";
-            this.textBox10.Size = new System.Drawing.Size(39, 13);
+            this.textBox10.Size = new System.Drawing.Size(52, 15);
             this.textBox10.TabIndex = 10;
             this.textBox10.Text = "Barras";
             // 
@@ -249,9 +299,10 @@
             // 
             // btmTeste
             // 
-            this.btmTeste.Location = new System.Drawing.Point(815, 10);
+            this.btmTeste.Location = new System.Drawing.Point(1087, 12);
+            this.btmTeste.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btmTeste.Name = "btmTeste";
-            this.btmTeste.Size = new System.Drawing.Size(75, 23);
+            this.btmTeste.Size = new System.Drawing.Size(100, 28);
             this.btmTeste.TabIndex = 3;
             this.btmTeste.Text = "Testar";
             this.btmTeste.UseVisualStyleBackColor = true;
@@ -259,56 +310,26 @@
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(1124, 10);
+            this.progressBar1.Location = new System.Drawing.Point(1499, 12);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(121, 23);
+            this.progressBar1.Size = new System.Drawing.Size(161, 28);
             this.progressBar1.TabIndex = 4;
-            // 
-            // zedGraphControl1
-            // 
-            this.zedGraphControl1.BackColor = System.Drawing.Color.RosyBrown;
-            this.zedGraphControl1.Location = new System.Drawing.Point(40, 3);
-            this.zedGraphControl1.Name = "zedGraphControl1";
-            this.zedGraphControl1.ScrollGrace = 0D;
-            this.zedGraphControl1.ScrollMaxX = 0D;
-            this.zedGraphControl1.ScrollMaxY = 0D;
-            this.zedGraphControl1.ScrollMaxY2 = 0D;
-            this.zedGraphControl1.ScrollMinX = 0D;
-            this.zedGraphControl1.ScrollMinY = 0D;
-            this.zedGraphControl1.ScrollMinY2 = 0D;
-            this.zedGraphControl1.Size = new System.Drawing.Size(1165, 291);
-            this.zedGraphControl1.TabIndex = 11;
-            this.zedGraphControl1.UseExtendedPrintDialog = true;
-            // 
-            // zedGraphControl2
-            // 
-            this.zedGraphControl2.BackColor = System.Drawing.Color.RosyBrown;
-            this.zedGraphControl2.Location = new System.Drawing.Point(39, 300);
-            this.zedGraphControl2.Name = "zedGraphControl2";
-            this.zedGraphControl2.ScrollGrace = 0D;
-            this.zedGraphControl2.ScrollMaxX = 0D;
-            this.zedGraphControl2.ScrollMaxY = 0D;
-            this.zedGraphControl2.ScrollMaxY2 = 0D;
-            this.zedGraphControl2.ScrollMinX = 0D;
-            this.zedGraphControl2.ScrollMinY = 0D;
-            this.zedGraphControl2.ScrollMinY2 = 0D;
-            this.zedGraphControl2.Size = new System.Drawing.Size(1165, 289);
-            this.zedGraphControl2.TabIndex = 12;
-            this.zedGraphControl2.UseExtendedPrintDialog = true;
             // 
             // formInterface
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.ClientSize = new System.Drawing.Size(1264, 661);
+            this.ClientSize = new System.Drawing.Size(1685, 814);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.btmTeste);
             this.Controls.Add(this.btmConectar);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.tabControl1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "formInterface";
@@ -343,8 +364,8 @@
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Button btmTeste;
         private System.Windows.Forms.ProgressBar progressBar1;
-        private ZedGraph.ZedGraphControl zedGraphControl2;
-        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private ZedGraph.ZedGraphControl graphFFT;
+        private ZedGraph.ZedGraphControl graphBars;
     }
 }
 
